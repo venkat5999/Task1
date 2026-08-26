1b1d37bfb8618e792df23369815ab4c38ddfad38
venkat5999 <83050809+venkat5999@users.noreply.github.com>
2026-08-26 23:23:29 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,20 @@
+// 1.Create a variable for student name, age, and mark and print all three.
+   var studentName = "venkat chodisetti";
+   var age = 26;
+   var mark = 100;
+   console.log(studentName)
+   console.log(age)
+   console.log(mark)
+//2.Ask the user's name using prompt() and display it using alert().
+   let name = prompt("Enter your name:");
+      alert(name);
+//3.Ask the user's age using prompt() and print it using console.log().
+    let userAge = prompt("enter your age:");
+    console.log(userAge)
+//4.Ask the user a question using confirm().
+      let question="Do you want to continue ?";
+       confirm(question);
+
+//5.Ask the user's name and display it on the webpage using document.writeln().
+     var  userNamee=prompt("enter your Username :");
+       document.write(userNamee);
